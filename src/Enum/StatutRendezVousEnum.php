dd1aa75b0e987f46b5e4faf44251b89d572d9bf3
Ihsane-12 <benminaihssane@gmail.com>
2026-06-05 12:47:15 +0100

    Enum pour les statuts des rendez-vous  Correspond à la colonne 'statut' de la table 'rendez_vous'

---
+++ src/Enum/StatutRendezVousEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enum;
+
+
+class StatutRendezVousEnum
+{
+    const EN_ATTENTE = 'En attente';
+    const CONFIRME = 'Confirmé';
+    const ANNULE = 'Annulé';
+    const TERMINE = 'Terminé';
+}

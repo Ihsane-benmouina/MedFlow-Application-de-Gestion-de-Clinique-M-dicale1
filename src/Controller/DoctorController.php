4831e8418b43e1ca575f231f3567ac355c0b8e04
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-05 12:23:09 +0100

    Créer le class de DoctorRepository en utilisat la connexion comme attribute

--- src/Controller/DoctorController.php
+++ src/Controller/DoctorController.php
@@ -3,3 +3,14 @@
 namespace App\Repository;
 
 use PDO;
+
+class SpecialiteRepository
+{
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+}

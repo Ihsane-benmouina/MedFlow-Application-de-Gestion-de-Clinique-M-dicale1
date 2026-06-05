3a028d0f112121f9a997b61bc32f24bf1cf33d4e
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-05 12:45:17 +0100

    Ajouter la fonction de findAll

--- src/Repository/MedecinRepository.php
+++ src/Repository/MedecinRepository.php
@@ -12,4 +12,16 @@ class MedecinRepository
     {
         $this->pdo = $pdo;
     }
+
+        public function findAll(): array
+    {
+        $sql = "SELECT m.id as id_medecin, m.actif, m.id_specialite,
+                       u.id as id_user, u.nom, u.prenom, u.email,
+                       s.nom as specialite_nom
+                FROM medecins m
+                JOIN users u ON m.id_user = u.id
+                JOIN specialites s ON m.id_specialite = s.id
+                ORDER BY u.nom ASC";
+        return $this->pdo->query($sql)->fetchAll();
+    }
 }
